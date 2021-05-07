f2c97b3cc45d2b4e5b0767dc0083178e89309ee8
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-07 11:29:05 +0100

    Update scanImage.sh

--- files/scanImage.sh
+++ files/scanImage.sh
@@ -2,13 +2,12 @@
 
 curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
 sudo chmod a+x ./twistcli
-try {
-		sh 'docker pull pasqu4le/evilpetclinic:latest'       
-    sh 'curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli'
-    sh 'sudo chmod a+x ./twistcli'
+ {
+    docker pull pasqu4le/evilpetclinic:latest     
+    curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
     sh "./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details pasqu4le/evilpetclinic"        
-        } catch (err) {
-            echo err.getMessage()
+        
+ } || {
             echo "Error detected"
-			throw RuntimeException("Build failed for some specific reason!")
-        }
+	    echo "Build failed for some specific reason!"
+      }
