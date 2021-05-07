6eee2926aa277e95e4e39dd30ccd8107f5896165
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-07 11:21:15 +0100

    Create scanImage.sh

---
+++ files/scanImage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -k -u $PC_USER:$PC_PASS --output ./twistcli https://$PC_CONSOLE/api/v1/util/twistcli
+sudo chmod a+x ./twistcli
+try {
+		sh 'docker pull pasqu4le/evilpetclinic:latest'       
+    sh 'curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli'
+    sh 'sudo chmod a+x ./twistcli'
+    sh "./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details pasqu4le/evilpetclinic"        
+        } catch (err) {
+            echo err.getMessage()
+            echo "Error detected"
+			throw RuntimeException("Build failed for some specific reason!")
+        }
