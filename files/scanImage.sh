6f2ccd913a4437888fd1572e21e2fe88c1c8590e
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-07 11:31:55 +0100

    Update scanImage.sh

--- files/scanImage.sh
+++ files/scanImage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
-sudo chmod a+x ./twistcli
+chmod a+x ./twistcli
  {
     docker pull pasqu4le/evilpetclinic:latest     
     curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
-    sh "./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details pasqu4le/evilpetclinic"        
+    ./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details pasqu4le/evilpetclinic
         
  } || {
             echo "Error detected"
