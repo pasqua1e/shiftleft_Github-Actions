fe0e382a7c143a2626a24dc3aedf42906e60a416
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-19 14:55:51 +0100

    Update repoScan.sh

--- files/repoScan.sh
+++ files/repoScan.sh
@@ -3,4 +3,4 @@
 
 curl -k -u $TL_USER:$TL_PASS "https://$TL_CONSOLE/api/v1/util/twistcli" -v -o twistcli
 chmod +x twistcli
-./twistcli coderepo scan --address https://console-master-pasq.prussiello.demo.twistlock.com -u $TL_USER -p $TL_PASS .  
+./twistcli coderepo scan --address https://$TL_CONSOLE -u $TL_USER -p $TL_PASS .  
