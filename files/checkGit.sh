15bbd88e31b35617b53d2b6f330e21d4ba2600c8
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-19 14:26:45 +0100

    Update checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -l
+
+curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' -X POST https://$TL_CONSOLE/api/v1/coderepos/scan
+sleep 5
 REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
   https://$TL_CONSOLE/api/v1/coderepos?id=pasqua1e%2Fevil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
