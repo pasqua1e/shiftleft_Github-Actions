f931dc3c7fb8e4467270681d28998dcd99db458c
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-05-20 14:05:51 +0100

    Update repoScan.sh

--- files/repoScan.sh
+++ files/repoScan.sh
@@ -3,9 +3,10 @@
 curl -k -u $TL_USER:$TL_PASS "https://$TL_CONSOLE/api/v1/util/twistcli" -v -o twistcli
 chmod +x twistcli
 ./twistcli coderepo scan --address https://$TL_CONSOLE -u $TL_USER -p $TL_PASS ./app  
-result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
+#result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
 
 ./twistcli coderepo scan --address https://$TL_CONSOLE -u $TL_USER -p $TL_PASS ./license  
+result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
 
 
 if [ "$result" == "true" ]; then
