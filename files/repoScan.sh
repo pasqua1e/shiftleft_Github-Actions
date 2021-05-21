46cc4bdacfc75ccc19c932410077b17bc4883b5b
pasq <p.russiello@libero.it>
2021-05-21 20:19:49 +0100

    app folder reposcan

--- files/repoScan.sh
+++ files/repoScan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-./twistcli coderepo scan --address https://$TL_CONSOLE -u $TL_USER -p $TL_PASS ./app/ --excluded-paths ./license  
+./twistcli coderepo scan --address https://$TL_CONSOLE -u $TL_USER -p $TL_PASS ./app 
 result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
 
 if [ "$result" == "true" ]; then
