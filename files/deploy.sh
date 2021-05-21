da97364ec10cf4276c295b792fe8d4a3ca08780a
pasq <p.russiello@libero.it>
2021-05-21 12:46:59 +0100

    deplo script fix

--- files/deploy.sh
+++ files/deploy.sh
@@ -1,6 +1,6 @@
-!/bin/bash
+#!/bin/bash
 export KUBECONFIG=/var/www/.kube/config
 kubectl create ns evil
-kubectl delete --ignore-not-found=true -f /var/www/html/deploy.yml -n evil'
+kubectl delete --ignore-not-found=true -f /var/www/html/deploy.yml -n evil
 kubectl apply -f /var/www/html/deploy.yml -n evil
 sleep 20
