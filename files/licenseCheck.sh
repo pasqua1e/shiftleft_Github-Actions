c75f0b0468dbe6a6663ab47f3e918becd22909d7
pasq <p.russiello@libero.it>
2021-05-21 12:26:07 +0100

    reposc changes

--- files/licenseCheck.sh
+++ files/licenseCheck.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -l
 
-./twistcli coderepo scan --address https://$TL_CONSOLE -u $TL_USER -p $TL_PASS ./app  
+./twistcli coderepo scan --address https://$TL_CONSOLE -u $TL_USER -p $TL_PASS ./license  
 result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
 
+
 if [ "$result" == "true" ]; then
-   echo "Code Repo scan passed!"
+   echo "License check passed!"
    exit 0
 else
-   echo "Code Repo scan failed!"
+   echo "License check failed!"
    exit 1
 fi
