dba8042e4b16cc01c8efef0be528d02f715d54ef
pasq <p.russiello@libero.it>
2021-05-25 19:32:06 +0100

    test (malware disabled)

--- files/scanImage.sh
+++ files/scanImage.sh
@@ -8,7 +8,7 @@ docker pull pasqu4le/evilpetclinic:latest
 ./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details pasqu4le/evilpetclinic -o output.json
 result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
 
-echo "\n"
+echo
 
 if [ -z "$result" ]; then
    echo "Image Vulnerability scan passed!"
