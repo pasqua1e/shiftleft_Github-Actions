b32ba0e26c599c02f0d0111a3bd281d6d57f4845
pasq <p.russiello@libero.it>
2021-05-26 15:16:46 +0100

    added WF malware runtime

--- files/runtime_attacks.sh
+++ files/runtime_attacks.sh
@@ -6,6 +6,8 @@ PO=$(kubectl get po -n evil|grep evil| awk '{ print $1 }')
 
 echo "DL bad evil file"
 kubectl exec $PO -n evil -- bash -c "curl https://cdn.twistlock.com/john/evil -o evil"
+kubectl exec $PO -n evil -it -- bash -c "curl https://wildfire.paloaltonetworks.com/publicapi/test/elf  -o WF-evil_$(date +%s)"
+
 
 echo "Kubernetes attack"
 kubectl exec $PO -n evil -- bash -c "curl https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl -o kubectl; chmod +x ./kubectl ; ./kubectl version --client"
