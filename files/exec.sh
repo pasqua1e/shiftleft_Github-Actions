f6cd2955a5aee9ac1b7796235d87db6d2c705fe6
pasq <p.russiello@libero.it>
2021-05-28 17:35:31 +0100

    simplified exec script

--- files/exec.sh
+++ files/exec.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 host=$(echo $TL_CONSOLE | sed -e "s/console/dvwa/")
-curl -s -u $TL_USER:$TL_PASS https://$host/login.php
-CSRF=$(curl -s -u $TL_USER:$TL_PASS -c dvwa.cookie "https://$host/login.php" | awk -F 'value=' '/user_token/ {print $2}' | cut -d "'" -f2)
-SESSIONID=$(grep PHPSESSID dvwa.cookie | awk -F '\t' '{print $7}')
-curl -s -b dvwa.cookie -u $TL_USER:$TL_PASS -d "username=admin&password=password&user_token=${CSRF}&Login=Login" "https://$host/login.php"
-SESSIONID=$(grep PHPSESSID dvwa.cookie | awk -F '\t' '{print $7}')
-curl -s -b dvwa.cookie -u $TL_USER:$TL_PASS "https://$host/exec.php?script=$1"
+curl -s -u $TL_USER:$TL_PASS "https://$host/exec.php?script=$1"
