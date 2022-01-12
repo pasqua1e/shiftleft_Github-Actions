fabdd938d9babd9a896d3052bde4f0ea89b23fd5
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2022-01-12 18:07:06 +0000

    added -k to curl in exec script

--- files/exec.sh
+++ files/exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 host=$(echo $TL_CONSOLE | sed -e "s/console/dvwa/")
-curl -s -u $TL_USER:$TL_PASS "https://$host/exec.php?script=$1"
+curl -k -s -u $TL_USER:$TL_PASS "https://$host/exec.php?script=$1"
